f601da550c9af9abce605053bc7fdf16d6f0e510
Sam Roberts <samr72003@gmail.com>
2026-03-17 13:58:36 -0600

    feat: implement tab navigation and add models for Game, Group, Location, Message, Profile, Report, and Sports

---
+++ LocalLegends/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions= {{ 
+      tabBarStyle: {
+          width: '90%',
+          height: 60,
+          borderRadius: 30,
+          bottom: 30,
+          position: 'absolute',
+          left: '50%',
+          marginLeft: '5%',
+          shadowColor: '#000000aa',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+      },
+      tabBarItemStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      tabBarActiveTintColor: '#6366f1',
+      tabBarInactiveTintColor: '#94a3b8',
+      headerShown: false 
+    }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <Ionicons name="search" size={28} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
