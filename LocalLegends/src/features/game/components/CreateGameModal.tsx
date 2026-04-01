ba8ad8ae15bfb3604d470208575e92da17579bba
Sam Roberts <samr72003@gmail.com>
2026-04-01 12:58:05 -0600

    style: add vertical spacing in CreateGameModal for improved layout

--- LocalLegends/src/features/game/components/CreateGameModal.tsx
+++ LocalLegends/src/features/game/components/CreateGameModal.tsx
@@ -33,6 +33,7 @@ export default function CreateGameModal({ isVisible, setIsCreateGameModalVisible
                             </View>
 
                             <CreateGameForm />
+
                         </View>
                     </TouchableWithoutFeedback>
                 </View>
